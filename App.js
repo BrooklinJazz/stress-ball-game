e5a68607be6950cef766850168e7cccc6082db3d
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-02-23 18:39:54 -0700

    Create Static Game

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-
-import styled from "styled-components/native"
+import React, { useState } from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const BaseText = styled.Text`
   color: ${props => props.color};
-`
+`;
 
 const BaseButton = styled.TouchableOpacity`
   justify-content: center;
@@ -20,24 +20,62 @@ const BaseButton = styled.TouchableOpacity`
   min-width: 100px;
 `;
 
-export const Button = ({children, color, ...props}) => (
+export const Button = ({ children, color, ...props }) => (
   <BaseButton {...props}>
-    <BaseText color={color} >{children}</BaseText>
+    <BaseText color={color}>{children}</BaseText>
   </BaseButton>
 );
 
 export const success = "#43a047";
 
 export const SuccessButton = styled(Button).attrs(props => ({
-  color: "white",
+  color: "white"
 }))`
   background-color: ${success};
 `;
 
+const GameState = {
+  playing: "playing",
+  starting: "starting"
+};
+
+const StressContainer = styled.View`
+  border: solid 1px black;
+  width: 40px;
+  height: 255px;
+  justify-content: flex-end;
+`;
+
+const StressLevel = styled.View(
+  props => `
+  height: ${props.stressLevel}px;
+  background-color: rgb(${props.stressLevel}, ${255 - props.stressLevel}, 0);
+`
+);
+
+const StressBar = ({ stressLevel }) => {
+  return (
+    <StressContainer>
+      <StressLevel stressLevel={stressLevel} />
+    </StressContainer>
+  );
+};
+
+const Game = () => {
+  return (
+    <StressBar stressLevel={30}/>
+  );
+};
+
 export default function App() {
+  const [gameState, setGameState] = useState(GameState.starting);
+  const startGame = () => setGameState(GameState.playing);
   return (
     <Container>
-      <SuccessButton>Start</SuccessButton>
+      {gameState === GameState.starting && (
+        <SuccessButton onPress={startGame}>Start</SuccessButton>
+      )}
+      {gameState === GameState.playing && <Game />}
     </Container>
   );
 }
